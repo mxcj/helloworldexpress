fa3aaa7bb0ef425c7ab33f7f76a7623f78821993
Marcos Castañeda <mxcj@hotmail.com>
2023-05-28 00:47:20 -0500

    Change Mongodb IP connection

--- config/mongo.js
+++ config/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const dbConnect = () => {
-    const DB_URI = 'mongodb://3.143.217.135:27017/admin';
+    const DB_URI = 'mongodb://35.87.97.216:27017/admin';
     mongoose.connect(DB_URI,{
         user: "admin",
         pass: "Passwd123",
